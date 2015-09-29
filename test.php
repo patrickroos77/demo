967f54724039bc598292c5be64e1a6ee44024c75
Patrick Roos <patrick.roos@aoe.com>
2015-09-29 11:29:01 +0200

    test

--- test.php
+++ test.php
@@ -1,3 +1,44 @@
 <?php
-echo 'I am a test file';
+class thisIsJustATestClass{
+    /**
+     * @var string
+     */
+    private $privateVar;
 
+    /**
+     * @var string
+     */
+    public $publicVar;
+
+    /**
+     * @return string
+     */
+    public function getPrivateVar()
+    {
+        return $this->privateVar;
+    }
+
+    /**
+     * @param string $privateVar
+     */
+    public function setPrivateVar($privateVar)
+    {
+        $this->privateVar = $privateVar;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPublicVar()
+    {
+        return $this->publicVar;
+    }
+
+    /**
+     * @param string $publicVar
+     */
+    public function setPublicVar($publicVar)
+    {
+        $this->publicVar = $publicVar;
+    }
+}
